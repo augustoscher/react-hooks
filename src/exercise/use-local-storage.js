dba4b58ff5678a5318bc27aac6206e9cbe195e84
Augusto S. Scher <augusto@jusbrasil.com.br>
2023-02-17 13:50:28 -0300

    feat: custom hooks - epic react

---
+++ src/exercise/use-local-storage.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+export const useLocalStorage = (key, defaultValue = '') => {
+  const [state, setState] = React.useState(
+    () => window.localStorage.getItem(key) ?? defaultValue,
+  )
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, state)
+  }, [key, state])
+
+  return [state, setState]
+}
