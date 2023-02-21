d0abcd1adc0486104dd3a7a8a530ce52c2e6a065
Augusto S. Scher <augusto@jusbrasil.com.br>
2023-02-21 09:31:35 -0300

    feat: custom hooks - epic react

--- src/exercise/use-local-storage.js
+++ src/exercise/use-local-storage.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-export const useLocalStorage = (key, defaultValue = '') => {
+export const useLocalStorageV1 = (key, defaultValue = '') => {
   const [state, setState] = React.useState(
     () => window.localStorage.getItem(key) ?? defaultValue,
   )
@@ -8,6 +8,39 @@ export const useLocalStorage = (key, defaultValue = '') => {
   React.useEffect(() => {
     window.localStorage.setItem(key, state)
   }, [key, state])
+
+  return [state, setState]
+}
+
+export const useLocalStorage = (
+  key,
+  defaultValue = '',
+  { serialize = JSON.stringify, deserialize = JSON.parse } = {},
+) => {
+  const [state, setState] = React.useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key)
+    if (valueInLocalStorage) {
+      // the try/catch is here in case the localStorage value was set before
+      // we had the serialization in place (like we do in previous extra credits)
+      try {
+        return deserialize(valueInLocalStorage)
+      } catch (error) {
+        window.localStorage.removeItem(key)
+      }
+    }
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+  })
+
+  const prevKeyRef = React.useRef(key)
+
+  React.useEffect(() => {
+    const prevKey = prevKeyRef.current
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey)
+    }
+    prevKeyRef.current = key
+    window.localStorage.setItem(key, serialize(state))
+  }, [key, state, serialize])
 
   return [state, setState]
 }
