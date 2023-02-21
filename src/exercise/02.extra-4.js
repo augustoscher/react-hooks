7190e80a1d50ea98b60fffe8b72e1caaeace340e
Augusto S. Scher <augusto@jusbrasil.com.br>
2023-02-21 09:31:41 -0300

    feat: custom hooks - epic react

---
+++ src/exercise/02.extra-4.js
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import { useLocalStorage } from './use-local-storage'
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorage('name', initialName)
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+function App() {
+  return <Greeting />
+}
+
+export default App
