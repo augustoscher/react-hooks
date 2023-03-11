4078fff20420fb52392b9671fcbd48172d88233b
Augusto S. Scher <augusto@jusbrasil.com.br>
2023-03-11 12:54:16 -0300

    feat: hooks - colocate state - epic react

---
+++ src/exercise/03.extra-1.js
@@ -0,0 +1,45 @@
+// Lifting state
+// http://localhost:3000/isolated/exercise/03.js
+
+import * as React from 'react'
+
+function Name({name, onNameChange}) {
+  return (
+    <div>
+      <label htmlFor="name">Name: </label>
+      <input id="name" value={name} onChange={onNameChange} />
+    </div>
+  )
+}
+
+function FavoriteAnimal() {
+  const [animal, setAnimal] = React.useState('')
+  return (
+    <div>
+      <label htmlFor="animal">Favorite Animal: </label>
+      <input
+        id="animal"
+        value={animal}
+        onChange={event => setAnimal(event.target.value)}
+      />
+    </div>
+  )
+}
+
+function Display({ name, animal }) {
+  return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
+}
+
+function App() {
+  const [name, setName] = React.useState('')
+
+  return (
+    <form>
+      <Name name={name} onNameChange={event => setName(event.target.value)} />
+      <FavoriteAnimal />
+      <Display name={name} />
+    </form>
+  )
+}
+
+export default App
